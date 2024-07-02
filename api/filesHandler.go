ab546ea2464137adb44bd399ec18f7ef2a028e9f
JuanJDlp <juanjosedelapava1@gmail.com>
2024-07-01 19:35:43 -0500

    Implemented the first version of saving a file using http

--- api/filesHandler.go
+++ api/filesHandler.go
@@ -1,7 +1,35 @@
 package api
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+
+	"github.com/JuanJDlp/File_Storage_System/internal"
+	"github.com/labstack/echo/v4"
+)
 
 type FilesHanlder struct {
-	e *echo.Echo
+	e       *echo.Echo
+	storage *internal.Storage
+}
+
+func (fh *FilesHanlder) Start() {
+	fh.e.POST("/api/v1/files", fh.saveFile)
+}
+
+func (fh *FilesHanlder) saveFile(c echo.Context) error {
+	file, err := c.FormFile("file")
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	src, err := file.Open()
+
+	fh.storage.SaveFile(file.Filename, src)
+
+	fh.e.Logger.Print(file)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	return c.JSON(http.StatusOK, "ok")
 }
--- api/router.go
+++ api/router.go
@@ -1,6 +1,9 @@
 package api
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/JuanJDlp/File_Storage_System/internal"
+	"github.com/labstack/echo/v4"
+)
 
 type router struct {
 	e            *echo.Echo
@@ -9,13 +12,17 @@ type router struct {
 
 func NewRouter() *router {
 	e := echo.New()
+	storage := internal.NewStorage(0)
+	filesHadler := &FilesHanlder{e, storage}
 
 	return &router{
 		e,
-		&FilesHanlder{e},
+		filesHadler,
 	}
+
 }
 
 func (r *router) Start(port string) error {
+	r.filesHandler.Start()
 	return r.e.Start(":" + port)
 }
