088657659dce41221522916f6e5b2ac8d0a984e3
JuanJDlp <juanjosedelapava1@gmail.com>
2024-07-01 20:51:57 -0500

    Implemented the first version of delete and get endpoint

--- api/filesHandler.go
+++ api/filesHandler.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/JuanJDlp/File_Storage_System/internal"
 	"github.com/labstack/echo/v4"
@@ -14,6 +15,8 @@ type FilesHanlder struct {
 
 func (fh *FilesHanlder) Start() {
 	fh.e.POST("/api/v1/files", fh.saveFile)
+	fh.e.DELETE("/api/v1/files/:name", fh.deleteFile)
+	fh.e.GET("/api/v1/files/:name", fh.dowloadFile)
 }
 
 func (fh *FilesHanlder) saveFile(c echo.Context) error {
@@ -26,10 +29,37 @@ func (fh *FilesHanlder) saveFile(c echo.Context) error {
 
 	fh.storage.SaveFile(file.Filename, src)
 
-	fh.e.Logger.Print(file)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, err)
 	}
 
 	return c.JSON(http.StatusOK, "ok")
+}
+
+func (fh *FilesHanlder) deleteFile(c echo.Context) error {
+	fileName := c.Param("name")
+	err := fh.storage.Delete(fileName)
+	if err != nil {
+		fh.e.Logger.Print(err.Error())
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, "ok")
+}
+
+func (fh *FilesHanlder) dowloadFile(c echo.Context) error {
+	fileName := c.Param("name")
+	size, file, err := fh.storage.ReadFile(fileName)
+	sizeString := strconv.Itoa(int(size))
+	if err != nil {
+		fh.e.Logger.Print(err.Error())
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	// Set the appropriate headers
+	c.Response().Header().Set(echo.HeaderContentDisposition, "attachment; filename="+fileName)
+	c.Response().Header().Set(echo.HeaderContentType, "application/octet-stream")
+	c.Response().Header().Set(echo.HeaderContentLength, sizeString)
+
+	// Stream the file content to the response
+	return c.Stream(http.StatusOK, "application/octet-stream", file)
 }
